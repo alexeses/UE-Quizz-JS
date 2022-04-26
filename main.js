b8a88e91d85e7e6b9aef3725a16807bc1d0232a7
Alex Gheorghe <alex@digitalsynware.com>
2022-04-26 23:31:01 +0200

    more fixed
    + field name
    + check if field name is complete
    + add color in select field
    + add goood answers and quest
    + code improvement
    + add github pages

--- main.js
+++ main.js
@@ -1,29 +1,48 @@
-let opcCorrecta = [3,3,3,1,1]
+let opcCorrecta = [1,3,1,1,1]
 let opcElegida = []
 let cantOK = 0
+let colorSelect = "#f1f1f1cd";
+let colorNone = "#3bc17a";
 
 function respuesta(nPregunta, select) {
   opcElegida[nPregunta] = select.value;
 
   id = "p" + nPregunta
 
+  console.log(id)
+  console.log(select.value)
+
   labels = document.getElementById(id).childNodes;
-  labels[3].style.setbackgroundColor("red");
-  labels[5].style.setbackgroundColor("red");
-  labels[7].style.setbackgroundColor("red");
+  labels[3].style.backgroundColor = colorSelect;
+  labels[5].style.backgroundColor = colorSelect;
+  labels[7].style.backgroundColor = colorSelect;
   
-  select.parentNode.style.backgroundColor("green");
+  select.parentNode.style.backgroundColor = colorNone;
+}
+
+function userIsNotEmpty() {
+  let user = document.getElementById("user").value;
+  if (user.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function corregir() {
-  cantOK = 0
 
-  for (let i = 0; i < opcElegida.length; i++) {
-    if (opcElegida[i] == opcCorrecta[i]) {
-      cantOK++;
+  if (userIsNotEmpty() == true) {
+    cantOK = 0
+
+    for (let i = 0; i < opcElegida.length; i++) {
+      if (opcElegida[i] == opcCorrecta[i]) {
+        cantOK++;
+      }
     }
-  } 
+    document.getElementById("resultado").innerHTML = "Respuestas correctas: " + cantOK;
+  } else {
+    document.getElementById("resultado").innerHTML = "No has ingresado tu nombre";
+  }
 
-  document.getElementById("resultado").innerHTML = cantOK;
 
 }
